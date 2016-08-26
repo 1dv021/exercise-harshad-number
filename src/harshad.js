d58dc05d13c99835789114fe7806a972a400493c
mtslck <mats.loock@lnu.se>
2016-08-26 11:33:53 +0200

    Initial commit

---
+++ src/harshad.js
@@ -0,0 +1,52 @@
+/**
+ * Namespace for Harshad numbers.
+ *
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+let Harshad = (function() {
+
+  return {
+    /**
+    * Returns true when the given number is a valid Harshad number.
+    *
+    * @param {Number} number The given number.
+    * @returns {Boolean}
+    */
+    isValid: function(number) {
+
+      // TODO: Write your code here! This is the first function to implement.
+
+    },
+
+    /**
+    * Gets the next Harshad number after the given number.
+    *
+    * @param {Number} number The given number.
+    * @returns {Number}
+    */
+    getNext: function(number) {
+
+      // TODO: Write your code here! This is the second function to implement.
+
+    },
+
+    /**
+    * Returns a sequence of Harshad numbers, starting after a given number.
+    *
+    * @param {Number} count The number of consecutive Harshad numbers to return.
+    * @param {Number} [start = 0] The number after which the sequence should start; defaults to 0.
+    * @returns {Array.<Number>}
+    */
+    getSequence: function(count, start = 0) {
+
+      // TODO: Write your code here! This is the third function to implement.
+
+    }
+  };
+}());
+
+module.exports = Harshad;
